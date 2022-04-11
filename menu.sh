4dd668c373e1ad039d90bfc253d89e7690e00a2f
Afiniel <afitrade@hotmail.com>
2022-04-11 23:38:42 +0200

    Fix Menu.sh bug

---
+++ menu.sh
@@ -0,0 +1,38 @@
+##################################################
+# Source code https://github.com/end222/pacmenu  #
+# Updated by Afiniel for crypto use...           #
+##################################################
+
+source /etc/functions.sh
+cd $HOME/yiimpool/yiimp_upgrade
+
+RESULT=$(dialog --stdout --nocancel --default-item 1 --title "YiiMP Pool Updater v4.4" --menu "Choose one" -1 65 7 \
+' ' "- Upgrade Stratum and YiiMP Single Server Installation -" \
+1 "Upgrade YiiMP Single Server Installation" \
+' ' "- Upgrade YiiMP Web Server Multi Server Installation -" \
+2 "Upgrade Web Server - Run on web server" \
+3 Exit)
+if [ $RESULT = ]
+then
+bash $(basename $0) && exit;
+fi
+
+if [ $RESULT = 1 ]
+then
+clear;
+cd $HOME/yiimpool/yiimp_upgrade
+source single.sh;
+fi
+
+if [ $RESULT = 2 ]
+then
+clear;
+cd $HOME/yiimpool/yiimp_upgrade
+source up_web.sh
+fi
+
+if [ $RESULT = 3 ]
+then
+clear;
+exit;
+fi
